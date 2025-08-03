99386489db535eab89afc3a753f536fbbcf4a1f8
AvishekAdhikary <141558136+AvishekAdhikary@users.noreply.github.com>
2025-08-03 16:35:01 +0545

    initial commit

---
+++ src/components/footer.js
@@ -0,0 +1,11 @@
+import styles from './footer.module.css';
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <p>&copy; 2023 My Company</p>
+      <p>Follow us on social media!</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/header.js
@@ -0,0 +1,22 @@
+// import"./header.css";
+import styles from './header.module.css';
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <nav>
+        <ul className={styles.list}>
+          <li className={styles['list-item']}>
+            <a href='#'>Home</a>
+          </li>
+          <li className={styles['list-item']}>
+            <a href='#'>Contact</a>
+          </li>
+          <li className={styles['list-item']}>
+            <a href='#'>About</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+export default Header;
